54c4b181745f530dacd5c17a9211a44e6bed8ae5
Brian1011 <mutindabrian26@gmail.com>
2018-12-03 20:47:49 +0300

    Create the default homepage, that is phonebook.blade.php

---
+++ resources/views/phonebook.blade.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Brian Mutinda
+ * Date: 03/12/2018
+ * Time: 08:36 PM
+ */
+?>
+<html>
+    <head>
+        <title>PhoneBook App</title>
+        <link rel="stylesheet" href="{{asset('css/app.css')}}">
+        <script src="{{asset('js/app.js')}}"></script>
+    </head>
+
+    <body>
+        <div id="app">
+
+        </div>
+    </body>
+</html>
